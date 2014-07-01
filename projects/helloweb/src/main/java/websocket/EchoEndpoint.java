4737993d04271b158d0fdcc458da72e5ce99f1e8
Takashi Sasaki <t.sasaki@bmandw.com>
2014-07-01 12:59:06 +0900

    annotated endpoint

--- projects/helloweb/src/main/java/websocket/EchoEndpoint.java
+++ projects/helloweb/src/main/java/websocket/EchoEndpoint.java
@@ -2,15 +2,14 @@ package websocket;
 
 import java.io.IOException;
 import javax.websocket.OnMessage;
-import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
 @ServerEndpoint("/echo")
 public class EchoEndpoint {
 
     @OnMessage
-    public void onMessage(Session session, String message) throws IOException {
-        session.getBasicRemote().sendText("From server: " + message);
+    public String onMessage(String message) throws IOException {
+        return "From server::: " + message;
     }
     
 }
