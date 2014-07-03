029245ae20ea9d08e93325e405a0cf9251f369be
Takashi Sasaki <t.sasaki@bmandw.com>
2014-07-03 12:05:43 +0900

    ClientEndpointアノテーションが2つ付いているミスを修正

--- projects/helloweb/src/test/java/websocket/EchoEndpointTest.java
+++ projects/helloweb/src/test/java/websocket/EchoEndpointTest.java
@@ -14,7 +14,6 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 
-@ClientEndpoint
 public class EchoEndpointTest {
     @Test
     public void testOnMessage() throws Exception {
